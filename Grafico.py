0fbce85e9de518c5c11e9a1c4da7e8bb736bc8ab
GMSSR <79996340+GMSSR@users.noreply.github.com>
2025-07-18 18:12:38 -0300

    Update Grafico.py

--- Grafico.py
+++ Grafico.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import re
 
+#Extrai os thoughputs dos arquivos iperf gerados pela competição
 def parse_throughput(file_path):
     throughput_data = []
     bandwidth_re = re.compile(r'([\d\.]+)\s*(M|K)bits/sec')
@@ -30,11 +31,11 @@ def parse_throughput(file_path):
 
     return throughput_data
 
-file_connection_1 = 'iperf_h1_reno.txt'
-file_connection_2 = 'iperf_h2_bbr.txt'
+reno_c = 'iperf_h1_reno.txt'
+bbr_c = 'iperf_h2_bbr.txt'
 
-throughput_1 = parse_throughput(file_connection_1)
-throughput_2 = parse_throughput(file_connection_2)
+throughput_1 = parse_throughput(reno_c)
+throughput_2 = parse_throughput(bbr_c)
 
 min_length = min(len(throughput_1), len(throughput_2))
 throughput_1 = throughput_1[:min_length]
@@ -48,17 +49,16 @@ for i in range(len(throughput_1) - 1):
     x0, y0 = throughput_1[i], throughput_2[i]
     dx = throughput_1[i + 1] - x0
     dy = throughput_2[i + 1] - y0
-    plt.arrow(x0, y0, dx, dy, head_width=0.1, head_length=0.1, fc='blue', ec='blue', length_includes_head=True)
+    plt.arrow(x0, y0, dx, dy, head_width=0.3, head_length=0.3, fc='red', ec='red', length_includes_head=True)
 
 x_vals = [0, max_throughput]
 y_vals = [max_throughput, 0]
 
-plt.plot(x_vals, y_vals, label=f"Max Throughput Line: x + y = {max_throughput}", color='r')
-plt.plot(x_vals, x_vals, '--')
+plt.plot(x_vals, y_vals, '--', label=f"Full bandwidth utilization line", color='black')
+plt.arrow(x = 0, y = 0, dx=max_throughput, dy=max_throughput,head_width=0.3, head_length=0.3, color='black', label=f"Equal bandwidth share")
 
-plt.xlabel("Throughput of Connection 1 (Mbps)")
-plt.ylabel("Throughput of Connection 2 (Mbps)")
-plt.title("Throughput Through Shared Bottleneck Over Time")
+plt.xlabel("Connection 1 Throughput (Mbps) (Reno)")
+plt.ylabel("Connection 2 Throughput (Mbps) (BBR)")
 
 plt.legend()
 
